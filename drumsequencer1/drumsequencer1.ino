d1d9983d6a062cdd72aef457ffbf9d4021966a48
Steven-Law <91934619+steven-law@users.noreply.github.com>
2022-08-27 19:45:34 +0200

    8-channel polymadness
    first step is one note above.
    So the kick C3 has its first step-entry one notevalue plus C#3, for every note...
    Only the first step has this issue!!

--- drumsequencer1/drumsequencer1.ino
+++ drumsequencer1/drumsequencer1.ino
@@ -15,6 +15,8 @@ ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 
 unsigned long previousMillis = 0;
 const long interval = 200;
+int barClock = 0;
+
 
 // This is calibration data for the raw touch data to the screen coordinates
 #define TS_MINX 180
@@ -39,6 +41,14 @@ const long interval = 200;
 #define SEQ_GRID_BOTTOM 12
 #define GRID_LENGTH_HOR 256
 #define GRID_LENGTH_VERT 192
+#define POSITION_SCALE_BUTTON 18
+#define POSITION_LOAD_BUTTON 15
+#define POSITION_SAVE_BUTTON 13
+#define POSITION_BPM_BUTTON 11
+#define POSITION_STOP_BUTTON 10
+#define POSITION_PLAY_BUTTON 8
+#define POSITION_RECORD_BUTTON 7
+#define POSITION_BAR_BUTTON 5
 #define DOT_OFFSET_X 40  //STEP_FRAME_W * 2 + 8
 #define DOT_OFFSET_Y 24  //STEP_FRAME_H + 8
 #define DOT_RADIUS 5
@@ -60,8 +70,6 @@ const long interval = 200;
 int trackColor[8] {6150246, 3326604, 1095334, 8678659, 6003265, 7678932, 12943157, 8044207};
 int step_Frame_X;
 int step_Frame_Y;
-int touchX;
-int touchY;
 int gridTouchX;            //decided to handle touchthingys with a grid, so here it is, 20 grids
 int gridTouchY;            //decided to handle touchthingys with a grid, so here it is, 15 grids
 
@@ -71,6 +79,8 @@ int gridTouchY;            //decided to handle touchthingys with a grid, so here
 bool scaleSelect = LOW;
 bool songSelect = LOW;     // a handler to keep the mode active after selecting
 bool channel1Select = LOW;     // a handler to keep the mode active after selecting
+
+
 bool channel2Select = LOW;     // a handler to keep the mode active after selecting
 bool channel3Select = LOW;     // a handler to keep the mode active after selecting
 bool channel4Select = LOW;     // a handler to keep the mode active after selecting
@@ -111,7 +121,11 @@ int scales[scalesQuant][12] {  //bool array for greying out notes that are not i
 char* scaleNames[scalesQuant] = {"Chromatic", "Major", "Natural Minor", "Harmonic Minor", "Melodic Minor", "Dorian", "Mixolydian", "Phrygian", "Lydian"}; // Scale Names for selecting
 char* scaleNamesShort[scalesQuant] = {"Chrom", "Major", "NatMi", "HarMi", "MelMi", "Dor", "Mixol", "Phryg", "Lyd"}; // Scale Names for selecting
 
-int sixteenthStep;
+
+
+
+
+byte velocity = 96;
 
 void setup() {
   Serial.begin(38400);
@@ -130,6 +144,9 @@ void setup() {
 
 void loop() {
   usbMIDI.read();
+
+
+
   showCoordinates ();
   TS_Point p = ts.getPoint();
   if (ts.touched()) {
@@ -339,12 +356,8 @@ void loop() {
 
 void startUpScreen () {   //static Display rendering
   tft.fillScreen(ILI9341_DARKGREY);
-  
-  tft.setTextColor(ILI9341_WHITE);
+
   tft.setFont(Arial_9);
-  tft.drawRect(STEP_FRAME_W * 10 - 2, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE);
-  tft.setCursor(STEP_FRAME_W * 10 + 2, 3);
-  tft.print("128");
 
   tft.setTextColor(ILI9341_BLACK);
   tft.fillRect(1, 1, 15, 16, ILI9341_MAGENTA);  //Xmin, Ymin, Xlength, Ylength, color
@@ -355,10 +368,10 @@ void startUpScreen () {   //static Display rendering
   tft.print("D");
   tft.fillRect(1, STEP_FRAME_H * 2, 15, STEP_FRAME_H, trackColor[1]); //Xmin, Ymin, Xlength, Ylength, color
   tft.setCursor(4, 34);
-  tft.print("M");
+  tft.print("2");
   tft.fillRect(1, STEP_FRAME_H * 3, 15, STEP_FRAME_H, trackColor[2]); //Xmin, Ymin, Xlength, Ylength, color
   tft.setCursor(4, 50);
-  tft.print("C");
+  tft.print("3");
   tft.fillRect(1, STEP_FRAME_H * 4, 15, STEP_FRAME_H, trackColor[3]); //Xmin, Ymin, Xlength, Ylength, color
   tft.setCursor(4, 66);
   tft.print("4");
@@ -374,33 +387,57 @@ void startUpScreen () {   //static Display rendering
   tft.fillRect(1, STEP_FRAME_H * 8, 15, STEP_FRAME_H, trackColor[7]); //Xmin, Ymin, Xlength, Ylength, color
   tft.setCursor(4, 130);
   tft.print("8");
-  tft.drawRect(STEP_FRAME_W * 18, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE);
-  tft.drawRect(STEP_FRAME_W * 8 - 2, 0, STEP_FRAME_W, STEP_FRAME_H, ILI9341_WHITE);
-  tft.setTextColor(ILI9341_GREEN);
-  //  tft.setFont(AwesomeF000_10);
-  tft.setCursor(STEP_FRAME_W * 8, 1);
-  tft.print("K");
-  tft.drawRect(STEP_FRAME_W * 9 - 2, 0, STEP_FRAME_W, STEP_FRAME_H, ILI9341_WHITE); //STOP RECT FRAME
-  tft.fillRect(STEP_FRAME_W * 9+1, 3, 10, 10, ILI9341_LIGHTGREY);  //STOP RECT
-  tft.drawRect(STEP_FRAME_W * 7 - 2, 0, STEP_FRAME_W, STEP_FRAME_H, ILI9341_WHITE);  // RECORD CIRCLE FRAME
-  tft.fillCircle(STEP_FRAME_W * 7 + 5, 7, DOT_RADIUS + 1, ILI9341_RED); // RECORD CIRCLE
+
+
+  tft.drawRect(STEP_FRAME_W * POSITION_SCALE_BUTTON, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE); //SCALE SELECT
+
+  tft.drawRect(STEP_FRAME_W * POSITION_RECORD_BUTTON - 2, 0, STEP_FRAME_W, STEP_FRAME_H, ILI9341_WHITE);  // RECORD CIRCLE FRAME
+  tft.fillCircle(STEP_FRAME_W * POSITION_RECORD_BUTTON + 5, 7, DOT_RADIUS + 1, ILI9341_RED); // RECORD CIRCLE
+
+  tft.drawRect(STEP_FRAME_W * POSITION_PLAY_BUTTON - 2, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE); //PLAY RECT FRAME
+  tft.fillTriangle(STEP_FRAME_W * POSITION_PLAY_BUTTON + 10, 3, STEP_FRAME_W * POSITION_PLAY_BUTTON + 10, 13, STEP_FRAME_W * POSITION_PLAY_BUTTON + 20, 8, ILI9341_GREEN); //PLAY TRIANGLE x1, y1, x2, y2, x3, y3
+
+
+  tft.drawRect(STEP_FRAME_W * POSITION_STOP_BUTTON - 2, 0, STEP_FRAME_W, STEP_FRAME_H, ILI9341_WHITE); //STOP RECT FRAME
+  tft.fillRect(STEP_FRAME_W * POSITION_STOP_BUTTON + 1, 3, 10, 10, ILI9341_LIGHTGREY); //STOP RECT
+
+  tft.drawRect(STEP_FRAME_W * POSITION_BAR_BUTTON - 2, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE); //BAR RECT FRAME
+  tft.drawRect(STEP_FRAME_W * POSITION_BPM_BUTTON - 2, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE); //BPM RECT FRAME
+
+  tft.drawRect(STEP_FRAME_W * POSITION_SAVE_BUTTON - 2, 0, STEP_FRAME_W * 2, STEP_FRAME_H, ILI9341_WHITE); //SAVE RECT FRAME
+  tft.fillRect(STEP_FRAME_W * POSITION_SAVE_BUTTON - 1, 1, STEP_FRAME_W * 2 - 2, STEP_FRAME_H - 2, ILI9341_ORANGE); //SAVE RECT FRAME
+  tft.setTextColor(ILI9341_BLACK);
+  tft.setFont(Arial_9);
+  tft.setCursor(STEP_FRAME_W * POSITION_SAVE_BUTTON, 3);
+  tft.print("SAV");
+
+  tft.drawRect(STEP_FRAME_W * POSITION_LOAD_BUTTON - 2, 0, STEP_FRAME_W, STEP_FRAME_H, ILI9341_WHITE); //LOAD RECT FRAME
+  tft.fillRect(STEP_FRAME_W * POSITION_LOAD_BUTTON - 1, 1, STEP_FRAME_W - 2, STEP_FRAME_H - 2, ILI9341_GREENYELLOW); //LOAD RECT FRAME
+  tft.setTextColor(ILI9341_BLACK);
+  tft.setFont(Arial_9);
+  tft.setCursor(STEP_FRAME_W * POSITION_LOAD_BUTTON, 3);
+  tft.print("L");
 
 }
 void showCoordinates () {
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    tft.fillRect(20, 0, 80, 13, ILI9341_DARKGREY);  //Xmin, Ymin, Xlength, Ylength, color
+    tft.fillRect(20, 0, 50, 13, ILI9341_DARKGREY);  //Xmin, Ymin, Xlength, Ylength, color
     tft.setTextColor(ILI9341_GREEN);
     tft.setFont(Arial_10);
     tft.setCursor(20, 3);
     tft.print("X");
     tft.print(gridTouchX);
-    tft.setCursor(50, 3);
+    tft.setCursor(45, 3);
     tft.print("Y");
     tft.print(gridTouchY);
-//    tft.setCursor(80, 3);
-//    tft.print(sixteenthClock);
+
+    tft.fillRect(STEP_FRAME_W * POSITION_BAR_BUTTON - 1, 2, STEP_FRAME_W * 2 - 2, STEP_FRAME_H - 3, ILI9341_DARKGREY);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setFont(Arial_9);
+    tft.setCursor(STEP_FRAME_W * POSITION_BAR_BUTTON + 2, 3);
+    tft.print(barClock);
   }
 }
 
@@ -409,26 +446,26 @@ void gridScaleSelector () {   //static Display rendering
   tft.drawRect(STEP_FRAME_W * 2, STEP_FRAME_H, STEP_FRAME_W * 16 , STEP_FRAME_H * 12, ILI9341_WHITE); //Xmin, Ymin, Xlength, Ylength, color
 }
 void scaleSelector () {
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-  for (int i = 0; i < scalesQuant; i++) {
-    tft.setCursor(STEP_FRAME_W * 2, STEP_FRAME_H * i + STEP_FRAME_H);
-    tft.setFont(Arial_8);
-    tft.setTextColor(ILI9341_WHITE);
-    tft.setTextSize(1);
-    tft.print(scaleNames[i]);
-
-    if (gridTouchX > 1 && gridTouchX < 15 && gridTouchY == i + 1) {
-      scaleSelected = i;
-      tft.fillRect(STEP_FRAME_W * 18 + 1, 1, STEP_FRAME_W * 2 - 2 , STEP_FRAME_H - 2, ILI9341_DARKGREY); //Xmin, Ymin, Xlength, Ylength, color
-      tft.setCursor(STEP_FRAME_W * 18 + 2, 2);
+    for (int i = 0; i < scalesQuant; i++) {
+      tft.setCursor(STEP_FRAME_W * 2, STEP_FRAME_H * i + STEP_FRAME_H);
       tft.setFont(Arial_8);
       tft.setTextColor(ILI9341_WHITE);
       tft.setTextSize(1);
-      tft.print(scaleNamesShort[scaleSelected]);
+      tft.print(scaleNames[i]);
+
+      if (gridTouchX > 1 && gridTouchX < 15 && gridTouchY == i + 1) {
+        scaleSelected = i;
+        tft.fillRect(STEP_FRAME_W * 18 + 1, 1, STEP_FRAME_W * 2 - 2 , STEP_FRAME_H - 2, ILI9341_DARKGREY); //Xmin, Ymin, Xlength, Ylength, color
+        tft.setCursor(STEP_FRAME_W * 18 + 2, 2);
+        tft.setFont(Arial_8);
+        tft.setTextColor(ILI9341_WHITE);
+        tft.setTextSize(1);
+        tft.print(scaleNamesShort[scaleSelected]);
+      }
     }
-  }
   }
 }
 
--- drumsequencer1/midiClockHandling.ino
+++ drumsequencer1/midiClockHandling.ino
@@ -1,18 +1,48 @@
+
 int countClock = 0;
 int sixteenthClock = 0;
 int fourthClock = 0;
-int barClock = 0;
 int phraseClock = 0;
 byte songPositionY = 232;
 byte stepPositionY = 236;
 byte thickness = 3;
-
+int bpm = 0;
+byte setNoteOn;
+byte noteOnClock;
+byte zaehler = 0;
+int zeitAlt = 0;
 //24ppqn  6ppqn=sixteenthClock  24ppqn=fourthClock  96ppqn=barClock 384ppqn=phraseClock
 void myClock() {
+  noInterrupts();
   countClock++;
+  noteOnClock++;
+  if (countClock % 24 == 0) {
+    bpm = ((60000 / (millis() - zeitAlt)));
+    Serial.print(bpm);
+    Serial.println(" BPM");
+    zeitAlt = millis();
+
+    tft.fillRect(STEP_FRAME_W * POSITION_BPM_BUTTON, 3, STEP_FRAME_W * 2 - 3, 10, ILI9341_DARKGREY);
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setFont(Arial_9);
+    tft.setCursor(STEP_FRAME_W * POSITION_BPM_BUTTON + 2, 3);
+    tft.print(bpm);
+
+  }
 
   if (countClock % 6 == 0) {
     sixteenthClock++;
+    noteOnClock = 0;
+    for (byte i = 0; i < 127; i++) {
+      if (channel1Clip[ch1Clip][i][sixteenthClock] == HIGH) {
+        usbMIDI.sendNoteOn(i, velocity, 10);
+      }
+
+      if (noteOnClock == 5) {
+        usbMIDI.sendNoteOff(i, velocity, 10);
+      }
+
+    }
 
     for (int songPointerThickness = 0; songPointerThickness <= thickness; songPointerThickness++) {
       for (int stepwidth = 0; stepwidth < 16; stepwidth++) {
@@ -35,10 +65,14 @@ void myClock() {
   if (countClock % 96 == 0) {
     barClock++;
     fourthClock = 0;
+
     for (int songPointerThickness = 0; songPointerThickness <= thickness; songPointerThickness++) {
       tft.drawPixel(barClock + STEP_FRAME_W * 2, songPositionY + songPointerThickness , ILI9341_GREEN);
     }
-
+    if (barClock == 200) {
+      barClock = 1;
+      tft.fillRect(STEP_FRAME_W * 2, songPositionY - 5, STEP_FRAME_W * 20, 20, ILI9341_DARKGREY);
+    }
   }
   if (countClock % 384 == 0) {
     phraseClock++;
@@ -46,9 +80,11 @@ void myClock() {
   }
 }
 void myContinue() {
+  zaehler = 0;
+  zeitAlt = millis();
   countClock = 0;
   sixteenthClock = 0;
-  barClock = 0;
+  barClock = 1;
   fourthClock = 0;
   phraseClock = 0;
   tft.fillRect(STEP_FRAME_W * 2, songPositionY - 5, STEP_FRAME_W * 16, 20, ILI9341_DARKGREY);
@@ -57,7 +93,7 @@ void myContinue() {
 void myStop() {
   countClock = 0;
   sixteenthClock = 0;
-  barClock = 0;
+  barClock = 1;
   fourthClock = 0;
   phraseClock = 0;
   tft.fillRect(STEP_FRAME_W * 2, songPositionY - 5, STEP_FRAME_W * 16, 20, ILI9341_DARKGREY);
